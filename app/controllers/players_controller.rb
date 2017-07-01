0be5c571c9b2a80893244c3a9e25419056e848fc
Moboluwarin <kolaogunnaike@gmail.com>
2017-07-01 15:51:01 +0200

    editing the controlers  players

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,43 @@
+class PlayersController < ApplicationController
+  def index
+    @players = Player.all
+  end
+  def show
+     @player = Player.find(params[:id])
+  end
+  def new
+    @player = Player.new
+  end
+
+  def create
+    @player = Player.new(player_params)
+    puts " inside create method"
+
+    if @player.save
+      puts "inside the player save"
+       redirect_to @player
+    else
+       render 'new'
+    end
+  end
+
+  def edit
+    @player = Player.find(params[:id])
+  end
+
+    def update
+      @player = Player.find(params[:id])
+
+      if @player.update_attributes(player_params)
+         redirect_to @player
+      else
+         render 'edit'
+      end
+    end
+
+  private
+  def player_params
+    params.require(:player).permit(:name, :age, :image_url)
+  end
+
+end
