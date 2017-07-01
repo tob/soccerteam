904d8da90a1f59e1555849bc2b69ad5a11d2fed1
smilingkite <mail@katinkahesselink.net>
2017-07-01 12:22:08 +0200

    one-to-many :models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701094101) do
+ActiveRecord::Schema.define(version: 20170701095222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20170701094101) do
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "team_id"
+    t.index ["team_id"], name: "index_players_on_team_id"
   end
 
   create_table "teams", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema.define(version: 20170701094101) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "players", "teams"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-team1 = Team.create!( { name: "FC Utrecht", image_url: "https://..." } )
+Player.delete_all
+Team.delete_all
+
+#teams
+team1 = Team.create!( { name: "FC Utrecht", image_url: "https://..."  } )
 team2 = Team.create!( { name: "Barcelona",  image_url: "https://..." } )
 team3 = Team.create!( { name: "Chelsea",  image_url: "https://..." } )
 
-player1 = Player.create!( { name: "Mobo", age: 25, image_url: "https://..." } )
-player2 = Player.create!( { name: "Jerry", age: 30, image_url: "https://..." } )
-player3 = Player.create!( { name: "Bennie", age: 45, image_url: "https://..." } )
+teams_count = Team.all.length
+puts "#{teams_count} teams were created."
+
+#players
+Player.create!([
+{ name: "Mobo", age: 25, image_url: "https://...", team: team1 },
+{ name: "Jerry", age: 30, image_url: "https://..." , team: team2} ,
+{ name: "Bennie", age: 45, image_url: "https://...", team: team3 }
+])
+players_count = Player.all.length
+puts "#{players_count} players were created."
